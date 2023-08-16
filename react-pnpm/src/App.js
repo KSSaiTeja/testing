428375cb87e4fc954c7626c4118f659b61773635
KSSaiTeja <saitej4865@gmail.com>
2023-08-16 20:23:02 +0530

    test commit igonore this

---
+++ react-pnpm/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="grid grid-col-4 grid-rows-4 place-items-center ">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <textarea cols='30' rows='10' className="bg-black text-white hover:bg-white hover:text-black cursor-all-scroll"> </textarea>
+    </div>
+  );
+}
+
+export default App;
